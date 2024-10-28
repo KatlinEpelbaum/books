c8dc6075eb44936856199ee88b02e9a981c85dca
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2024-10-28 13:22:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require_once('./connection.php');
+
+$stmt = $pdo->query('SELECT * FROM books');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <ul>
+    <?php while ($row = $stmt->fetch()){   ?> 
+        <li>
+            <a href= './book.php?id=<?=$row['id']; ?>'>
+                <?= $row['title']; ?>
+            </a>
+        </li>
+    </ul>
+    <?php } ?>
+</body>
+</html>
