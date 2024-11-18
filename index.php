906b2dd1d53f0f4d0a54ad8f0af316f734e37af7
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2024-11-18 13:06:30 +0200

    search for title

--- index.php
+++ index.php
@@ -1,25 +1,36 @@
-    <?php
-    require_once('./connection.php');
+<?php
+require_once('./connection.php');
 
+$searchQuery = isset($_GET['search']) ? $_GET['search'] : '';
+
+if ($searchQuery) {
+    $stmt = $pdo->prepare('SELECT * FROM books WHERE title LIKE :search AND is_deleted = 0');
+    $stmt->execute(['search' => '%' . $searchQuery . '%']);
+} else {
     $stmt = $pdo->query('SELECT * FROM books WHERE is_deleted = 0');
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
+}
+?>
 
-        <ul>
-        <?php while ($book = $stmt->fetch()){   ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Books</title>
+</head>
+<body>
+    <form action="<?= $_SERVER['PHP_SELF']; ?>" method="GET">
+        <input type="text" name="search" placeholder="Search for books..." value="<?= htmlspecialchars($searchQuery); ?>">
+        <button type="submit">Search</button>
+    </form>
+    <ul>
+        <?php while ($book = $stmt->fetch()) { ?>
             <li>
-                <a href= './book.php?id=<?=$book['id']; ?>'>
-                    <?= $book['title']; ?>
+                <a href='./book.php?id=<?= $book['id']; ?>'>
+                    <?= htmlspecialchars($book['title']); ?>
                 </a>
             </li>
-        </ul>
         <?php } ?>
-    </body>
-    </html>
+    </ul>
+</body>
+</html>
